ceae16de337318e26d5f5277fc581d6e0835dc87
tamaoki <treasure6hunter9cole@gmail.com>
2020-05-17 16:16:08 +0900

    スタート画面のデザインを整える

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -12,20 +12,48 @@ class _StartPageState extends State<StartPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: FlatButton(
-          child: Text(
-            'Start',
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              new MaterialPageRoute<Null>(
-                settings: const RouteSettings(name: Constants.startCountRoute),
-                builder: (BuildContext context) => StartCountPage(),
-                fullscreenDialog: true, // ダイアログで表示するかどうか
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Numbers', style: TextStyle(
+              fontSize: 30,
+              color: Colors.white
+            ),),
+            SizedBox(
+              height: 123,
+            ),
+            Container(
+              width: 190,
+              height: 60,
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: Constants.borderGrayColor,
+                    width: 5.0
+                ),
+                borderRadius: BorderRadius.circular(5),
+                color: Constants.orangeColor,
               ),
-            );
-          },
+              child: FlatButton(
+                child: Text(
+                  'Game Start',style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white
+                ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute<Null>(
+                      settings: const RouteSettings(name: Constants.startCountRoute),
+                      builder: (BuildContext context) => StartCountPage(),
+                      fullscreenDialog: true, // ダイアログで表示するかどうか
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:twentyfive/utils/hex_color.dart';
+
 class Constants {
   static const String startRoute = '/start';
   static const String startCountRoute = '/start_count';
@@ -5,4 +8,7 @@ class Constants {
 
   static const double circleSize = 150.0;
   static const double circleFontSize = 50.0;
+
+  static Color orangeColor = HexColor('#FA6400');
+  static Color borderGrayColor = HexColor('#555555');
 }
